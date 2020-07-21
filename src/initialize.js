128f593628fb4a2fed54bd657605f16943575f0c
Pratik Lodha <pratiklodha01@gmail.com>
2020-07-21 09:55:56 +0530

    custom resolvers for Protein type

---
+++ src/initialize.js
@@ -0,0 +1,19 @@
+export const initializeDatabase = (driver) => {
+  // const initCypher = `CALL apoc.schema.assert({}, {User: ["userId"], Business: ["businessId"], Review: ["reviewId"], Category: ["name"]})`
+  // const initCypher = null;
+
+  const executeQuery = (driver) => {
+    const session = driver.session();
+    return session
+      .writeTransaction((tx) => tx.run())
+      .then()
+      .finally(() => session.close());
+  };
+
+  // executeQuery(driver).catch((error) => {
+  //   console.error(
+  //     "Database initialization failed to complete\n",
+  //     error.message
+  //   );
+  // });
+};
