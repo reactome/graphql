c5d2efe4624faf239f24515d91addd4922400987
Pratik Lodha <pratiklodha01@gmail.com>
2020-07-21 11:30:43 +0530

    changes in src and readme

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import neo4j from "neo4j-driver";
 import { makeAugmentedSchema } from "neo4j-graphql-js";
 import dotenv from "dotenv";
 import { initializeDatabase } from "./initialize";
-import { resolvers } from "./resolvers";
+//import { resolvers } from "./resolvers";
 
 // set environment variables from .env
 dotenv.config();
