e35478167c410ccab70e5130da58637cf7e91c0d
Pratik Lodha <pratiklodha01@gmail.com>
2020-07-22 19:18:04 +0530

    resolver for protein scalar values

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,16 +1,27 @@
 import reactionResolver from "./resolvers/reaction";
+import proteinResolver from "./resolvers/protein";
 
 export const resolvers = {
   Query: {
-      Reaction: (obj, args, context, info) => {
-          let session = context.driver.session(),
-            params = {dbId : args.dbId},
-            query = `MATCH (rle:ReactionLikeEvent) WHERE rle.dbId = $dbId RETURN rle`;
+    Reaction: (obj, args, context, info) => {
+      let session = context.driver.session(),
+        params = { dbId: args.dbId },
+        query = `MATCH (rle:ReactionLikeEvent) WHERE rle.dbId = $dbId RETURN rle`;
 
-          return session.run(query, params).then((result) => {
-            return result.records.map(rec => rec.get("rle"));
-          });
-      }
+      return session.run(query, params).then((result) => {
+        return result.records.map((rec) => rec.get("rle"));
+      });
+    },
+    Protein: (obj, args, context, info) => {
+      let session = context.driver.session(),
+        params = { dbId: args.dbId },
+        query = `MATCH (ewas:EntityWithAccessionedSequence) WHERE ewas.dbId = $dbId RETURN ewas`;
+
+      return session.run(query, params).then((result) => {
+        return result.records.map((rec) => rec.get("ewas"));
+      });
+    },
   },
-  Reaction: reactionResolver
+  Reaction: reactionResolver,
+  Protein: proteinResolver,
 };
