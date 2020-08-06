6eb8b31887882b59a6299916afda8915c89f602e
Pratik Lodha <pratiklodha01@gmail.com>
2020-08-06 11:42:00 +0530

    major - new pathway arguments and minor - protein entity name unwind

--- src/resolvers.js
+++ src/resolvers.js
@@ -42,12 +42,33 @@ const resolvers = {
     },
     Pathway: (parent, args, context, info) => {
       let session = context.driver.session(),
-        params = { dbId: args.dbId },
-        query = `MATCH (p:Pathway) WHERE p.dbId = $dbId RETURN p`;
+        params = { value: args.valueType === "DB_ID" ? Number(args.value) : args.value, valueType: args.valueType },
+        query = getQuery(args.valueType);
+
+      function getQuery(valueType) {
+        if (valueType === "DB_ID") {
+          return `MATCH (p:Pathway) WHERE p.dbId = $value RETURN p`;
+
+        } else if (valueType === "STABLE_ID") {
+          return `MATCH (p:Pathway) WHERE p.stId CONTAINS $value RETURN p`;
+
+        } else if (valueType === "NAME") {
+          return `MATCH (p:Pathway)
+          UNWIND p.name as pathwayNameList
+          WITH pathwayNameList, p
+          WHERE pathwayNameList CONTAINS $value
+          RETURN p`;
+
+        } else {
+          throw `Please make sure that value entered for ${valueType} is correct`
+        }
+      };
 
       return session.run(query, params).then((result) => {
-        const record = result.records[0].get("p");
-        return record;
+        return result.records.map((rec) => {
+          const record = rec.get("p");
+          return record;
+        })
       });
     },
     Protein: (parent, args, context, info) => {
@@ -57,16 +78,13 @@ const resolvers = {
 
       function getQuery(valueType) {
         if (valueType === "DB_ID") {
-          return `MATCH (ewas:EntityWithAccessionedSequence) 
-              WHERE ewas.dbId = $value RETURN ewas`;
+          return `MATCH (ewas:EntityWithAccessionedSequence) WHERE ewas.dbId = $value RETURN ewas`;
 
         } else if (valueType === "UNIPROT_IDENTIFIER") {
-          return `MATCH (ewas:EntityWithAccessionedSequence)-[:referenceEntity]->(rgp:ReferenceGeneProduct)
-              WHERE rgp.identifier CONTAINS $value
-              RETURN ewas`;
+          return `MATCH (ewas:EntityWithAccessionedSequence)-[:referenceEntity]->(rgp:ReferenceGeneProduct) WHERE rgp.identifier CONTAINS $value RETURN ewas`;
 
         } else if (valueType === "ENTITY_NAME") {
-          return `MATCH (ewas:EntityWithAccessionedSequence) WHERE ewas.name CONTAINS $value RETURN ewas`;
+          return `MATCH (ewas:EntityWithAccessionedSequence) UNWIND ewas.name as ewasNameList WITH ewasNameList, ewas WHERE ewasNameList CONTAINS $value RETURN ewas`;
 
         } else if (valueType === "GENE_NAME") {
           return `MATCH (ewas:EntityWithAccessionedSequence)-[:referenceEntity]->(rgp:ReferenceGeneProduct)
